984d349c69d3e34f79ee9cd62883ef41387f833c
J5T3R0R <1chgr077@gmail.com>
2025-12-26 21:54:19 +0100

    .

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,6 +1,25 @@
 global ft_read
 
+extern __errno_location
+
 section .text
 
+        ; ARGUMENTS:
+        ; rdi - file descriptor
+        ; rsi - buffer to read into
+        ; rdx - number of bytes to read
+
 ft_read:
-    xor rax, rax
+        mov     rax, 0          ; syscall number for read
+        syscall
+        cmp rax, 0
+        jl .error
+        ret
+
+.error:
+    neg rax
+    mov rdi, rax
+    call __errno_location
+    mov [rax], edi
+    mov rax, -1
+    ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,22 +1,28 @@
 global ft_write
 
+extern __errno_location
+
+section .text
+
+        ; ARGUMENTS:
+        ; rdi - file descriptor
+        ; rsi - buffer to write from
+        ; rdx - number of bytes to write
 
 ft_write:
-    xor rax, rax        ; rax to 0
-    cmp rdi, 0          ;cmp rdi with 0 if first arg (fd) is less than 0 return an error
-    jl .error
-    ; call syscall write();
     mov rax, 1
     syscall
+
     cmp rax, 0
-    jge .done                   ; if file descriptor is greater than or equal to 0 return the 
+    jl .error
+    ret
+
 
 .error:
-    extern __errno_location     ; Declare an external symbol
-    mov edi, 9                  ; 9 is num for Bad file number  errno
-    call __errno_location       ; returns a pointer to the thread-local errno in RAX
-    mov dword [rax], edi        ; here we change that value in the address to EBADF 9 using edi because errno is 32bit integer
-    mov rax, -1                 ; sets the rax to -1 so we return -1 like in write funtion.
-
-.done:
+    neg rax
+    mov rdi, rax
+    call __errno_location
+    mov [rax], edi
+    mov rax, -1
     ret
+
